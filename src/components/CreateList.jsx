4b51591e49535dfe6cba7207856b2668083c3c5d
geoffreykithuku <mutembeikithuku12@gmail.com>
2022-11-13 22:18:39 +0300

    changed bg color

--- src/components/CreateList.jsx
+++ src/components/CreateList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState , useReducer} from 'react'
 
 function CreateList({setTaskList}) {
     const [taskTitle, setTaskTitle] = useState(' ')
